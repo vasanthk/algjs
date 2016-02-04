ef54fef7ac2eafc4e25a2067a369581a7ac36f66
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-04 13:10:42 -0800

    Quick Sort

---
+++ quickSort.js
@@ -0,0 +1,57 @@
+/**
+ * Quick sort an array
+ *
+ * Time Complexity:
+ * Worst case: O(n^2)
+ * Average case: O(nlogn)
+ * Best case: O(nlogn)
+ *
+ * Space Complexity (Worst): O(nlogn)
+ *
+ */
+
+function quick_sort(arr) {
+  return quick_sort_helper(arr, 0, arr.length - 1);
+}
+
+function quick_sort_helper(arr, left, right) {
+  if (left >= right) return;
+
+  var pivot = partition(arr, left, right);
+
+  quick_sort_helper(arr, left, pivot - 1);
+  quick_sort_helper(arr, pivot + 1, right);
+}
+
+function partition(arr, left, right) {
+  var pivot = left,
+    pivot_value = arr[pivot];
+
+  while (left < right) {
+    while (arr[left] <= pivot_value) {
+      left++;
+    }
+    while (arr[right] > pivot_value) {
+      right--;
+    }
+    if (left < right) {
+      swap(arr, left, right);
+    }
+  }
+
+  arr[pivot] = arr[right];
+  arr[right] = pivot_value;
+
+  return right;
+}
+
+
+function swap(arr, left, right) {
+  var temp = arr[left];
+  arr[left] = arr[right];
+  arr[right] = temp;
+}
+
+x = [1, 2, 3, 8, 3, 4, 6, 8, 1, 1, 1, 2];
+quick_sort(x);
+console.log('Sorted Array', x);
