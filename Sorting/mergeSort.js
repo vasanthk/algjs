033fec5ac6e2b20ffb3a79205db0af927acbdcdd
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-09 22:56:19 -0800

    Merge sort updated

--- Sorting/mergeSort.js
+++ Sorting/mergeSort.js
@@ -9,39 +9,34 @@
  * Space Complexity (Worst): O(n)
  *
  * @Reference:
+ * http://khan4019.github.io/front-end-Interview-Questions/sort.html#mergeSort
  * https://github.com/chihungyu1116/interview_sols_javascript/blob/master/algorithm/sort_merge_sort.js
  */
 
-function merge_sort(arr) {
-  console.log('Sorted Array: ', merge_sort_helper(arr));
-}
-
-function merge_sort_helper(arr) {
-  var middle = parseInt(arr.length / 2);
-
-  if (arr.length < 2) return arr;
-
-  var left = merge_sort_helper(arr.slice(0, middle));
-  var right = merge_sort_helper(arr.slice(middle));
-
-  return merge(left, right);
-}
-
-function merge(a, b) {
-  var arr = [];
-  var a_index = 0, b_index = 0;
-  while (a_index < a.length && b_index < b.length) {
-    if (a[a_index] <= b[b_index]) {
-      arr.push(a[a_index]);
-      a_index++;
-    } else {
-      arr.push(b[b_index]);
-      b_index++;
-    }
+function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
   }
 
-  // Adds in the remaining elements from the larger array.
-  arr = arr.concat(a.slice(a_index)).concat(b.slice(b_index));
+  var middle = parseInt(arr.length / 2);
+  var left = arr.slice(0, middle);
+  var right = arr.slice(middle);
 
-  return arr;
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  var result = [],
+    l = 0,
+    r = 0;
+  while (l < left.length && r < right.length) {
+    if (left[l] < right[r]) {
+      result.push(left[l++]);
+    }
+    else {
+      result.push(right[r++]);
+    }
+  }
+  // Remaining part needs to be added to the result
+  return result.concat(left.slice(l)).concat(right.slice(r));
 }
