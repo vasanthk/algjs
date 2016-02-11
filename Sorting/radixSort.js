c848b05dda097ce3ba0ed9cddc336a3aa2892662
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-10 18:17:27 -0800

    Radix sort updated

--- Sorting/radixSort.js
+++ Sorting/radixSort.js
@@ -1,38 +1,66 @@
 /**
  * Radix Sort
  *
+ * https://github.com/naomiajacobs/radixSort/blob/master/radixSort.js
  * https://github.com/kellybhoward/radix_sort_Javascript/blob/master/radix_sort.js
+ * https://www.youtube.com/watch?v=YXFI4osELGU
  */
 
 function radixSort(array) {
-  var count = 0;
-  var sorted = false;
-  while (!sorted) {
-    var map = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: []}; //bucket to collect numbers
-    var size = array.length;
-    for (var i = 0; i < size; i++) {
-      var word = array[i].toString().split(""); //make 'word' array of individual numbers
-      if (word.length <= count) {
-        map[0].push(array[i]); //if decimal place is 0
-      } else {
-        map[word[(word.length) - (1) - count]].push(array[i]);
-      }
+  // find longest and split array int
+  var numOfRounds = 0;
+  var newArray = array.map(function (num) {
+    if (num.toString().length > numOfRounds) {
+      numOfRounds = num.toString().length;
+    }
+    return num.toString().split('');
+  });
 
-    }
-    var collector = [];
-    for (var index = 0; index <= 9; index++) {
-      for (var valueI = 0; valueI < map[index].length; valueI++) {
-        collector.push(map[index][valueI]);
+  // add zeros on as necessary now that we have the longest number
+  newArray.forEach(function (num) {
+    if (num.length < numOfRounds) {
+      var zerosToAdd = numOfRounds - num.length;
+      for (var i = 0; i < zerosToAdd; i++) {
+        num.unshift('0');
       }
     }
-    array = collector;
-    if (map[0].length == array.length) {
-      sorted = true;
-    }
-    count++;
+  });
+
+  // now each number is an array of numerals as strings
+
+  var buckets = [];
+
+  // sort array for each radix
+  for (var i = numOfRounds - 1; i > -1; i--) {
+    newArray.forEach(function (num) {
+      var radix = num[i];
+      // initialize bucket if not there already
+      buckets[radix] = buckets[radix] || [];
+      buckets[radix].push(num);
+    });
+
+    //concat them all back together
+    newArray = buckets.reduce(function (prev, curr) {
+      if (!curr) {
+        return prev;
+      }
+      return prev.concat(curr);
+    }, []);
+
+    // clear buckets at the end of the round
+    buckets = [];
   }
-  return array;
+
+  //concat numbers back together
+  newArray = newArray.map(function (num) {
+    return +num.reduce(function (prev, curr) {
+      return prev.concat(curr);
+    }, '');
+  });
+
+  return newArray;
 }
+
 var sampleArray = [4, 87, 21, 922, 45, 621, 900, 39, 2000];
 console.time('radix');
 console.log(radixSort(sampleArray));
