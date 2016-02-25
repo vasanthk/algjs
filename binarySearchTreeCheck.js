356fbd92996e43d2a3a33761f00c5d4c19012bd0
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-24 18:10:17 -0800

    BST check

--- binarySearchTreeCheck.js
+++ binarySearchTreeCheck.js
@@ -1,47 +1,26 @@
 /**
  * Check if given binary tree is a BST
  *
+ * @Reference:
+ * https://github.com/jokomo/moretoyproblems/blob/master/binary-search-tree-check.js
  */
 
-var Node = function (val) {
-  this.value = val;
-  this.left = null;
-  this.right = null;
-};
+function checkBST(bst) {
+  //max is the constraint on the left side of the tree
+  function checkTree(bst, min, max) {
+    if (!bst) {
+      return true;
+    }
 
-var root = new Node(5);
-var child1 = new Node(2);
-var child2 = new Node(7);
-var grandchild1 = new Node(1);
-var grandchild2 = new Node(3);
-var grandchild3 = new Node(6);
-var grandchild4 = new Node(8);
-root.left = child1;
-root.right = child2;
-child1.left = grandchild1;
-child1.right = grandchild2;
-child2.left = grandchild3;
-child2.right = grandchild4;
+    if (bst.value > max || bst.value <= min) {
+      return false;
+    }
 
-// Is the tree rooted at x a BST with all values strictly between min and max
-// (if min or max is null, treat as empty constraint)
-var isBinarySearchTree = function (node, min, max) {
-  if (node === null) {
+    if (!checkTree(bst.left, min, bst.value) || !checkTree(bst.right, bst.value, max)) {
+      return false;
+    }
     return true;
   }
 
-  if (min !== null && node.value < min) {
-    return false;
-  }
-
-  if (max !== null && node.value > max) {
-    return false;
-  }
-
-  return isBinarySearchTree(node.left, min, node.value) &&
-    isBinarySearchTree(node.right, node.value, max);
-};
-
-// console.log('Is it a Binary search tree?');
-// isBinarySearchTree(root, 1, 8);
-// true
+  return checkTree(bst, Number.MIN_VALUE, Number.MAX_VALUE);
+}
